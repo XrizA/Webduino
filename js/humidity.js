8d0669bc74be6abdbfb8afcfb80d6f8995837542
XrizA <25199860+XrizA@users.noreply.github.com>
2019-07-30 18:03:22 +0800

    Add files via upload

--- js/humidity.js
+++ js/humidity.js
@@ -1,228 +1,220 @@
-var name = "濕度";
+class humidity {
+    svgRoot;
+    name = "濕度";
+    value = 50;
+    gaugeMaxValue = 100;
+    chart;
+    width;
+    percentValue;
+    percent;
+    static repaintGauge;
+    static percToRad;
+    constructor(root) {
+        var margin, height, radius, barWidth, chartInset,
+            arc3, arc2, arc1, totalPercent, padRad, percent;
+        var degToRad, percToDeg;
 
-var value = parseInt((Math.random() * 101));
+        this.percentValue = this.value / this.gaugeMaxValue;
 
-var gaugeMaxValue = 100;
+        this.percent = this.percentValue;
 
-var percentValue = value / gaugeMaxValue;
+        this.svgRoot = d3.select(root);
 
-var needleClient;
+        margin = { top: 20, right: 40, bottom: 30, left: 40 };
 
-(function () {
+        this.width = this.svgRoot.node().offsetWidth - margin.left - margin.right;
+        height = this.width;
+        radius = Math.min(this.width, height) / 2;
+        barWidth = 40 * this.width / 300;
+        padRad = 0.025;
+        chartInset = 10;
 
-    var barWidth, chart, chartInset, degToRad, repaintGauge,
-        height, margin, numSections, padRad, percToDeg, percToRad,
-        percent, radius, sectionIndx, svg, totalPercent, width;
+        totalPercent = .75;
 
-    percent = percentValue;
+        percToDeg = perc => perc * 360;
 
-    numSections = 1;
-    sectionPerc = 1 / numSections / 2;
-    padRad = 0.025;
-    chartInset = 10;
+        degToRad = deg => deg * Math.PI / 180;
 
-    totalPercent = .75;
+        humidity.percToRad = perc => degToRad(percToDeg(perc));
 
-    el = d3.select("#humidity");
+        this.svgRoot = this.svgRoot.append("svg")
+            .attr("width", this.width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom);
 
-    margin = { top: 20, right: 40, bottom: 30, left: 40 };
+        this.chart = this.svgRoot.append("g")
+            .attr("transform", "translate(" + ((this.width + margin.left) / 2) + ", " +
+                ((height + margin.top) / 2) + ")");
 
-    width = el.node().offsetWidth - margin.left - margin.right;
-    height = width;
-    radius = Math.min(width, height) / 2;
-    barWidth = 40 * width / 300;
+        this.chart.append("path").attr("class", "arc chart-first");
+        this.chart.append("path").attr("class", "arc chart-second");
+        this.chart.append("path").attr("class", "arc chart-third");
 
-    percToDeg = perc => perc * 360;
+        arc3 = d3.arc()
+            .outerRadius(radius - chartInset)
+            .innerRadius(radius - chartInset - barWidth);
+        arc2 = d3.arc()
+            .outerRadius(radius - chartInset)
+            .innerRadius(radius - chartInset - barWidth);
+        arc1 = d3.arc()
+            .outerRadius(radius - chartInset)
+            .innerRadius(radius - chartInset - barWidth);
 
-    degToRad = deg => deg * Math.PI / 180;
-
-    percToRad = perc => degToRad(percToDeg(perc));
-
-    // Create SVG element
-    svg = el.append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom);
-
-    chart = svg.append("g")
-        .attr("transform", "translate(" + ((width + margin.left) / 2) + ", " +
-            ((height + margin.top) / 2) + ")");
-
-    chart.append("path").attr("class", "arc chart-first");
-    chart.append("path").attr("class", "arc chart-second");
-    chart.append("path").attr("class", "arc chart-third");
-
-    arc3 = d3.arc()
-        .outerRadius(radius - chartInset)
-        .innerRadius(radius - chartInset - barWidth)
-    arc2 = d3.arc()
-        .outerRadius(radius - chartInset)
-        .innerRadius(radius - chartInset - barWidth)
-    arc1 = d3.arc()
-        .outerRadius(radius - chartInset)
-        .innerRadius(radius - chartInset - barWidth)
-
-    repaintGauge = function () {
-        perc = 0.5;
-        var next_start = totalPercent;
-        arcStartRad = percToRad(next_start);
-        arcEndRad = arcStartRad + percToRad(perc / 3);
-        next_start += perc / 3;
+        humidity.repaintGauge = () => {
+            var perc = 0.5;
+            var next_start = totalPercent;
+            var arcStartRad = humidity.percToRad(next_start);
+            var arcEndRad = arcStartRad + humidity.percToRad(perc / 3);
+            next_start += perc / 3;
 
 
-        arc1.startAngle(arcStartRad).endAngle(arcEndRad);
+            arc1.startAngle(arcStartRad).endAngle(arcEndRad);
 
-        arcStartRad = percToRad(next_start);
-        arcEndRad = arcStartRad + percToRad(perc / 3);
-        next_start += perc / 3;
+            arcStartRad = humidity.percToRad(next_start);
+            arcEndRad = arcStartRad + humidity.percToRad(perc / 3);
+            next_start += perc / 3;
 
-        arc2.startAngle(arcStartRad + padRad).endAngle(arcEndRad);
+            arc2.startAngle(arcStartRad + padRad).endAngle(arcEndRad);
 
-        arcStartRad = percToRad(next_start);
-        arcEndRad = arcStartRad + percToRad(perc / 3);
+            arcStartRad = humidity.percToRad(next_start);
+            arcEndRad = arcStartRad + humidity.percToRad(perc / 3);
 
-        arc3.startAngle(arcStartRad + padRad).endAngle(arcEndRad);
+            arc3.startAngle(arcStartRad + padRad).endAngle(arcEndRad);
 
-        chart.select(".chart-first").attr('d', arc1);
-        chart.select(".chart-second").attr('d', arc2);
-        chart.select(".chart-third").attr('d', arc3);
-    }
+            this.chart.select(".chart-first").attr('d', arc1);
+            this.chart.select(".chart-second").attr('d', arc2);
+            this.chart.select(".chart-third").attr('d', arc3);
+        };
 
-    var dataset = [{ metric: name, value: value }]
+        var dataset = [{ metric: this.name, value: this.value }];
 
-    var texts = svg.selectAll("text")
-        .data(dataset)
-        .enter();
+        var texts = this.svgRoot.selectAll("text")
+            .data(dataset)
+            .enter();
 
-    texts.append("text")
-        .text(() => dataset[0].metric)
-        .attr("id", "Name")
-        .attr("transform", "translate(" + ((width + margin.left) / 2.35) + ", " +
-            ((height + margin.top) / 1.5) + ")")
-        .attr("font-weight", "bold")
-        .attr("font-size", 25);
+        texts.append("text")
+            .text(() => dataset[0].metric)
+            .attr("id", "Name")
+            .attr("transform", "translate(" + ((this.width + margin.left) / 2.35) + ", " +
+                ((height + margin.top) / 1.5) + ")")
+            .attr("font-weight", "bold")
+            .attr("font-size", 25);
 
-    var trX = width / 2 + 5;
-    if (value < 10) trX += 5;
-    var trY = width / 2 + 45;
+        var trX = this.width / 2 + 5;
+        if (this.value < 10) trX += 5;
+        var trY = this.width / 2 + 45;
 
-    displayValue = function () {
         texts.append("text")
             .text(() => dataset[0].value + "%")
             .attr("id", "Value")
             .attr("transform", "translate(" + trX + ", " + trY + ")")
             .attr("font-size", 18)
             .attr("font-weight", "bold")
-        //value = parseInt((Math.random() * 101));
-        //clearTimeout(displayValue)
+
+        texts.append("text")
+            .text(() => 0 + "%")
+            .attr("id", "")
+            .attr("transform", "translate(" + ((this.width + margin.left) / 100) + ", " +
+                ((height + margin.top) / 2) + ")")
+            .attr("font-size", 15)
+            .attr("font-weight", "bold")
+            .style("fill", "#000000");
+
+        texts.append("text")
+            .text(() => this.gaugeMaxValue / 2 + "%")
+            .attr("id", "scale10")
+            .attr("transform", "translate(" + ((this.width + margin.left) / 2.15) + ", " +
+                ((height + margin.top) / 30) + ")")
+            .attr("font-size", 15)
+            .attr("font-weight", "bold")
+            .style("fill", "#000000");
+
+        texts.append("text")
+            .text(() => this.gaugeMaxValue + "%")
+            .attr("id", "scale20")
+            .attr("transform", "translate(" + ((this.width + margin.left) / 1.08) + ", " +
+                ((height + margin.top) / 2) + ")")
+            .attr("font-size", 15)
+            .attr("font-weight", "bold")
+            .style("fill", "#fffff");
+    }
+};
+
+class Needle {
+
+    constructor(el, width) {
+        this.el = el;
+        this.len = width / 2.5;
+        this.radius = this.len / 8;
     }
 
-    texts.append("text")
-        .text(() => 0 + "%")
-        .attr("id", "")
-        .attr("transform", "translate(" + ((width + margin.left) / 100) + ", " +
-            ((height + margin.top) / 2) + ")")
-        .attr("font-size", 15)
-        .attr("font-weight", "bold")
-        .style("fill", "#000000");
+    recalcPointerPos = (perc) => {
+        var centerX, centerY, leftX, leftY, rightX, rightY, thetaRad, topX, topY;
+        thetaRad = humidity.percToRad(perc / 2);
+        centerX = centerY = 0;
+        topX = centerX - this.len * Math.cos(thetaRad);
+        topY = centerY - this.len * Math.sin(thetaRad);
+        leftX = centerX - this.radius * Math.cos(thetaRad - Math.PI / 2);
+        leftY = centerY - this.radius * Math.sin(thetaRad - Math.PI / 2);
+        rightX = centerX - this.radius * Math.cos(thetaRad + Math.PI / 2);
+        rightY = centerY - this.radius * Math.sin(thetaRad + Math.PI / 2);
 
-    texts.append("text")
-        .text(() => gaugeMaxValue / 2 + "%")
-        .attr("id", "scale10")
-        .attr("transform", "translate(" + ((width + margin.left) / 2.15) + ", " +
-            ((height + margin.top) / 30) + ")")
-        .attr("font-size", 15)
-        .attr("font-weight", "bold")
-        .style("fill", "#000000");
+        return "M " + leftX + " " + leftY + " L " + topX + " " + topY + " L " + rightX + " " + rightY;
+    };
 
-    texts.append("text")
-        .text(() => gaugeMaxValue + "%")
-        .attr("id", "scale20")
-        .attr("transform", "translate(" + ((width + margin.left) / 1.08) + ", " +
-            ((height + margin.top) / 2) + ")")
-        .attr("font-size", 15)
-        .attr("font-weight", "bold")
-        .style("fill", "#fffff");
+    render() {
+        this.el.append("circle")
+            .attr("class", "needle-center")
+            .attr("cx", 0)
+            .attr("cy", 0)
+            .attr("r", this.radius);
 
-    var Needle = (function () {
-        var recalcPointerPos = function (perc) {
-            var centerX, centerY, leftX, leftY, rightX, rightY, thetaRad, topX, topY;
-            thetaRad = percToRad(perc / 2);
-            centerX = centerY = 0;
-            topX = centerX - this.len * Math.cos(thetaRad);
-            topY = centerY - this.len * Math.sin(thetaRad);
-            leftX = centerX - this.radius * Math.cos(thetaRad - Math.PI / 2);
-            leftY = centerY - this.radius * Math.sin(thetaRad - Math.PI / 2);
-            rightX = centerX - this.radius * Math.cos(thetaRad + Math.PI / 2);
-            rightY = centerY - this.radius * Math.sin(thetaRad + Math.PI / 2);
+        return this.el.append("path")
+            .attr("class", "needle")
+            .attr("id", "client-needle")
+            .attr("d", this.recalcPointerPos.call(this, 0));
+    };
 
-            return "M " + leftX + " " + leftY + " L " + topX + " " + topY + " L " + rightX + " " + rightY;
-        };
+    moveTo(perc) {
+        var self,
+            oldValue = this.perc || 0;
 
-        function Needle(el) {
-            this.el = el;
-            this.len = width / 2.5;
-            this.radius = this.len / 8;
-        }
+        this.perc = perc;
+        self = this;
 
-        Needle.prototype.render = function () {
-            this.el.append("circle")
-                .attr("class", "needle-center")
-                .attr("cx", 0)
-                .attr("cy", 0)
-                .attr("r", this.radius);
+        this.el
+            .transition()
+            .delay(1000)
+            .ease(d3.easeQuad)
+            .duration(200)
+            .select(".needle")
+            .tween("reset-progress", () =>
+                (percentOfPercent) => {
+                    var progress = (1 - percentOfPercent) * oldValue;
+                    humidity.repaintGauge(progress);
 
-            return this.el.append("path")
-                .attr("class", "needle")
-                .attr("id", "client-needle")
-                .attr("d", recalcPointerPos.call(this, 0));
-        };
+                    return d3.select(this)
+                        .attr("d", this.recalcPointerPos.call(self, progress));
+                });
 
-        Needle.prototype.moveTo = function (perc) {
-            var self,
-                oldValue = this.perc || 0;
+        this.el
+            .transition()
+            .delay(300)
+            .ease(d3.easeBounce)
+            .duration(1500)
+            .select(".needle")
+            .tween("progress", () =>
+                (percentOfPercent) => {
+                    var progress = percentOfPercent * perc;
+                    humidity.repaintGauge(progress);
+                    return d3.select(this)
+                        .attr("d", this.recalcPointerPos.call(self, progress));
+                }
+            );
+    };
 
-            this.perc = perc;
-            self = this;
+}
 
-            this.el
-                .transition()
-                .delay(100)
-                .ease(d3.easeQuad)
-                .duration(200)
-                .select(".needle")
-                .tween("reset-progress", () =>
-                    function (percentOfPercent) {
-
-                        var progress = (1 - percentOfPercent) * oldValue;
-                        repaintGauge(progress);
-
-                        d3.select(this)
-                            .attr("d", recalcPointerPos.call(self, progress));
-                    });
-
-            this.el
-                .transition()
-                .delay(300)
-                .ease(d3.easeBounce)
-                .duration(1500)
-                .select(".needle")
-                .tween("progress", () =>
-                    function (percentOfPercent) {
-                        var progress = percentOfPercent * perc;
-                        repaintGauge(progress);
-                        return d3.select(this)
-                            .attr("d", recalcPointerPos.call(self, progress));
-                    }
-                );
-        };
-
-        return Needle;
-    })();
-
-    needle = new Needle(chart);
-    needle.render();
-    needle.moveTo(percent);
-
-    //setInterval(displayValue, 1000);
-
-})();
+var h = new humidity("#humidity");
+var n = new Needle(h.chart, h.width);
+n.render();
+n.moveTo(h.percent);
--- js/main.js
+++ js/main.js
@@ -4,12 +4,12 @@ var fan;
 const fanPin = 9;
 const fanPinMode = 3; // 0(數位輸入) , 1(數位輸出) , 2(類比輸入) , 3(類比輸出PWM)
 
-$(function () {
+$(() => {
     const thermometer = new Thermometer("#thermo");
     setInterval(() => thermometer.Update(parseInt(Math.random() * 56)), 1000);
 });
-
-/*boardReady({ device: "EVGpX", multi: true }, function(board){
+// "EVGpX"
+boardReady({ device: "Device ID", multi: true }, (board) => {
     board.systemReset();
     board.samplingInterval = 100;
     dht = getDht(board, dhtPin);
@@ -20,6 +20,6 @@ $(function () {
     dht.read((evt) => {
         thermometer.Update(dht.temperature);
     }, 1000);
-});*/
+});
 
 //console.log(thermometer);
